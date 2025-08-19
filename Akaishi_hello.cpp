a0b5ddfa53270b6505f0f03f727a8ca7cc8e5987
AkaishiTakumi <115193708+AkaishiTakumi@users.noreply.github.com>
2025-08-19 10:55:04 +0900

    [update] 2整数の比較をできるようにした
    二つの整数(a,b)を入力しa<bかa>=bを出力する

--- Akaishi_hello.cpp
+++ Akaishi_hello.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-	for(int i=0;i<10;++i) cout << "Hello World!" << endl;
+	int a, b;
+	cin >> a;
+	cin >> b;
+	if (a < b)
+		cout << a;
+		cout << "<" << b << endl;
+	else
+	{
+		cout << a;
+		cout << ">=" << b << endl;
+	}
+
+	for (int i = 0; i < 10; ++i)
+		cout << "Hello World!" << endl;
 	return 0;
 }
