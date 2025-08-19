a9c141d5518388b4f08c34b094e738170b0243f9
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-08-19 11:04:49 +0900

    無限ループに陥る問題を修正

--- Takata-SHO800.tsx
+++ Takata-SHO800.tsx
@@ -2,14 +2,12 @@
 const Home = () => {
 
     // ロジック部分
-    while (true) {
-       document.querySelectorAll("*").forEach((element) => { // すべてのDOMに対し
-            if (element instanceof HTMLElement) {  // 型チェック
-                console.log(element.getHTML()) // 要素のHTMLを取得して出力
-            }
-       })
-    }
-    
+    document.querySelectorAll("*").forEach((element) => { // すべてのDOMに対し
+        if (element instanceof HTMLElement) {  // 型チェック
+            console.log(element.getHTML()) // 要素のHTMLを取得して出力
+        }
+    })
+
     return (
         <main>
             hoge
