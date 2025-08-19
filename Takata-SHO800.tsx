04ba64414dd3ce562a938f5cd2e1e6e790841cdf
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-08-19 10:57:18 +0900

    Takata-SHO800.tsxにロジックを実装

--- Takata-SHO800.tsx
+++ Takata-SHO800.tsx
@@ -1,5 +1,15 @@
+"use client";
 const Home = () => {
 
+    // ロジック部分
+    while (true) {
+       document.querySelectorAll("*").forEach((element) => { // すべてのDOMに対し
+            if (element instanceof HTMLElement) {  // 型チェック
+                console.log(element.getHTML()) // 要素のHTMLを取得して出力
+            }
+       })
+    }
+    
     return (
         <main>
             hoge
