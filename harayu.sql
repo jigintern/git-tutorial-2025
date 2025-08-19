f5866c1992fe768f4ecdf53ca284418a8e66e642
yuma-harada <yu2ma1da7ha0ra@gmail.com>
2025-08-19 10:45:54 +0900

    add: Userテーブルの作成

--- harayu.sql
+++ harayu.sql
@@ -1,7 +1,14 @@
 CREATE TABLE Hello (
     id INTEGER AUTO_INCREMENT,
     message VARCHAR(16) NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(id) REFERENCES User(id)
+);
+
+CREATE TABLE User (
+    id INTEGER AUTO_INCREMENT,
+    name VARCHAR(16) UNIQUE,
+    PRIMARY KEY(id),  
 );
 
 INSERT INTO Hello(message) VALUES("Hello World!");
