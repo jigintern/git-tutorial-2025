43a7f501ab50f3831c7566f34411f00162cf3f39
kota oka <kouta.o.0702@gmail.com>
2025-08-19 10:51:45 +0900

    printStar関数を作成した

---
+++ tamago2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void printStar(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    
+    int n;
+    cin >> n;
+    printStar(n);
+    return 0;
+}
+
