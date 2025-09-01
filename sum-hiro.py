187cceba5f987c30a2f72df35ca7be009825df84
Hirotaka.U <hirotaka.with@gmail.com>
2025-09-01 14:41:01 +0900

    Update sum-hiro.py

--- sum-hiro.py
+++ sum-hiro.py
@@ -1,6 +1,9 @@
 def sum(a, b):
     return a + b
+def min(a, b):
+    return abs(a - b)
 
 A = int(input("Enter first number: "))
 B = int(input("Enter second number: "))
 print("The sum is:", sum(A, B))
+print("The absolute difference is:", min(A, B))
