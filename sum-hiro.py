c6c5f07ef93bd090988460fce9a10c9d5de9fb1e
Hirotaka.U <hirotaka.with@gmail.com>
2025-09-01 14:53:59 +0900

    絶対値の関数名変更と掛け算機能の追加

--- sum-hiro.py
+++ sum-hiro.py
@@ -1,9 +1,12 @@
 def sum(a, b):
     return a + b
-def min(a, b):
+def abDef(a, b):
     return abs(a - b)
+def mul(a, b):
+    return a * b
 
 A = int(input("Enter first number: "))
 B = int(input("Enter second number: "))
 print("The sum is:", sum(A, B))
-print("The absolute difference is:", min(A, B))
+print("The absolute difference is:", abDef(A, B))
+print("The multiply is:", mul(A, B))
